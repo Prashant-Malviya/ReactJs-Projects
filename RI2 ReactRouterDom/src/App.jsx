290fddaac09a9c5d7223d59d677e69f99e6254fa
Prashant Malviya <prashantmalviya272002@gmail.com>
2023-12-08 07:44:41 +0530

    Routing start

---
+++ RI2 ReactRouterDom/src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import './App.css'
+import { BrowserRouter, Route, Routes } from "react-router-dom"; 
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  
+
+  return (
+    
+   <div className="app">
+
+
+    <Routes>
+
+      <Route>
+        
+      </Route>
+
+    </Routes>
+
+   </div>
+  )
+}
+
+export default App
